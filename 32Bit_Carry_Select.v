2a7dcde98ebe5ea82c1f2d0fd5d737cd1cabe045
Kaustubh Kapil <kaustubhkapil2006@gmail.com>
2025-06-05 16:23:48 +0530

    Update 32Bit_Carry_Select.v

--- 32Bit_Carry_Select.v
+++ 32Bit_Carry_Select.v
@@ -5,47 +5,67 @@ module CSA32 (
     output [31:0] Sum,
     output        Cout
 );
-    wire [31:0] Sum0, Sum1;  
-    wire [3:0]  Carry0, Carry1; 
-    wire [3:0]  CarrySelect; 
+    wire [7:0] Sum0_0, Sum1_0;
+    wire [7:0] Sum0_1, Sum1_1;
+    wire [7:0] Sum0_2, Sum1_2;
+    wire [7:0] Sum0_3, Sum1_3;
 
-    // carries when Cin = 0
-    assign Carry0[0] = (A[7:0] & B[7:0]) | ((A[7:0] ^ B[7:0]) & 1'b0);
-    assign Sum0[7:0] = A[7:0] ^ B[7:0] ^ 1'b0;
+    wire       C0_0, C1_0;
+    wire       C0_1, C1_1;
+    wire       C0_2, C1_2;
+    wire       C0_3, C1_3;
 
-    assign Carry0[1] = (A[15:8] & B[15:8]) | ((A[15:8] ^ B[15:8]) & Carry0[0]);
-    assign Sum0[15:8] = A[15:8] ^ B[15:8] ^ Carry0[0];
+    reg        Sel1, Sel2, Sel3;
+    reg [31:0] Sum_internal;
+    reg        Cout_internal;
 
-    assign Carry0[2] = (A[23:16] & B[23:16]) | ((A[23:16] ^ B[23:16]) & Carry0[1]);
-    assign Sum0[23:16] = A[23:16] ^ B[23:16] ^ Carry0[1];
+    assign {C0_0, Sum0_0} = A[7:0] + B[7:0] + 1'b0;
+    assign {C1_0, Sum1_0} = A[7:0] + B[7:0] + 1'b1;
 
-    assign Carry0[3] = (A[31:24] & B[31:24]) | ((A[31:24] ^ B[31:24]) & Carry0[2]);
-    assign Sum0[31:24] = A[31:24] ^ B[31:24] ^ Carry0[2];
+    assign {C0_1, Sum0_1} = A[15:8] + B[15:8] + 1'b0;
+    assign {C1_1, Sum1_1} = A[15:8] + B[15:8] + 1'b1;
 
-    // carries when Cin = 1
-    assign Carry1[0] = (A[7:0] & B[7:0]) | ((A[7:0] ^ B[7:0]) & 1'b1);
-    assign Sum1[7:0] = A[7:0] ^ B[7:0] ^ 1'b1;
+    assign {C0_2, Sum0_2} = A[23:16] + B[23:16] + 1'b0;
+    assign {C1_2, Sum1_2} = A[23:16] + B[23:16] + 1'b1;
 
-    assign Carry1[1] = (A[15:8] & B[15:8]) | ((A[15:8] ^ B[15:8]) & Carry1[0]);
-    assign Sum1[15:8] = A[15:8] ^ B[15:8] ^ Carry1[0];
+    assign {C0_3, Sum0_3} = A[31:24] + B[31:24] + 1'b0;
+    assign {C1_3, Sum1_3} = A[31:24] + B[31:24] + 1'b1;
 
-    assign Carry1[2] = (A[23:16] & B[23:16]) | ((A[23:16] ^ B[23:16]) & Carry1[1]);
-    assign Sum1[23:16] = A[23:16] ^ B[23:16] ^ Carry1[1];
+    always @(*) begin
+        if (Cin) begin
+            Sum_internal[7:0] = Sum1_0;
+            Sel1 = C1_0;
+        end else begin
+            Sum_internal[7:0] = Sum0_0;
+            Sel1 = C0_0;
+        end
 
-    assign Carry1[3] = (A[31:24] & B[31:24]) | ((A[31:24] ^ B[31:24]) & Carry1[2]);
-    assign Sum1[31:24] = A[31:24] ^ B[31:24] ^ Carry1[2];
+        if (Sel1) begin
+            Sum_internal[15:8] = Sum1_1;
+            Sel2 = C1_1;
+        end else begin
+            Sum_internal[15:8] = Sum0_1;
+            Sel2 = C0_1;
+        end
 
-    // Select correct Carry based on Cin
-    assign CarrySelect[0] = Cin ? Carry1[0] : Carry0[0];
-    assign CarrySelect[1] = Cin ? Carry1[1] : Carry0[1];
-    assign CarrySelect[2] = Cin ? Carry1[2] : Carry0[2];
-    assign CarrySelect[3] = Cin ? Carry1[3] : Carry0[3];
+        if (Sel2) begin
+            Sum_internal[23:16] = Sum1_2;
+            Sel3 = C1_2;
+        end else begin
+            Sum_internal[23:16] = Sum0_2;
+            Sel3 = C0_2;
+        end
 
-    assign Sum[7:0]   = Cin ? Sum1[7:0] : Sum0[7:0];
-    assign Sum[15:8]  = CarrySelect[0] ? Sum1[15:8] : Sum0[15:8];
-    assign Sum[23:16] = CarrySelect[1] ? Sum1[23:16] : Sum0[23:16];
-    assign Sum[31:24] = CarrySelect[2] ? Sum1[31:24] : Sum0[31:24];
+        if (Sel3) begin
+            Sum_internal[31:24] = Sum1_3;
+            Cout_internal = C1_3;
+        end else begin
+            Sum_internal[31:24] = Sum0_3;
+            Cout_internal = C0_3;
+        end
+    end
 
-    assign Cout = CarrySelect[3];
+    assign Sum = Sum_internal;
+    assign Cout = Cout_internal;
 
 endmodule
